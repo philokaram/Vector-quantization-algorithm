f550e97d08377ec625630310b75c4f4af77c4d61
philokaram <philokrm@gmail.com>
2024-12-12 22:53:02 +0200

    aa

--- Main.java
+++ Main.java
@@ -1,4 +1,9 @@
+import java.io.File;
+import java.io.IOException;
 import java.util.Scanner;
+import java.awt.image.BufferedImage;
+import javax.imageio.ImageIO;
+
 class Main{
     public static void main(String[] args){
         System.out.println("    _________________________________________    ");
@@ -8,7 +13,11 @@ class Main{
         boolean isRunning = true;
         int choice;
         String imagePath;
+        int blockHight;
+        int blockWidth;
+        int codeBookSize;
         Scanner input = new Scanner(System.in);
+        VectorQuantization vectorQuantization = new VectorQuantization();
         while(isRunning){
             menu();
             choice = input.nextInt();
@@ -18,8 +27,19 @@ class Main{
                     imagePath = input.next();
                     System.out.println("Enter the block size: ");
                     System.out.print("  1-Enter the number of pixels for block width: ");
-                    System.out.print("  1-Enter the number of pixels for block hight: ");
+                    blockWidth = input.nextInt();
+                    System.out.print("  2-Enter the number of pixels for block hight: ");
+                    blockHight = input.nextInt();
                     System.out.print("Enter the code book size: ");
+                    codeBookSize = input.nextInt();
+                    File imageFile = new File(imagePath);
+                    try {
+                        BufferedImage image = ImageIO.read(imageFile);
+                        vectorQuantization.compress(image, blockWidth, blockHight, codeBookSize);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+
                 }
                 case 2 -> {
                 }
--- VectorQuantization.java
+++ VectorQuantization.java
@@ -1,3 +1,18 @@
+import java.awt.image.BufferedImage;
+
 class VectorQuantization{
-    
+    public void compress(BufferedImage image,int blockWidth , int blockHight , int codeBookSize ){
+        int[][] pixelsArray = new int[image.getHeight()][image.getWidth()];
+        System.out.println("w: "+image.getWidth()+"  h: "+image.getHeight());
+        for (int i = 1; i < image.getHeight(); i++) {
+            for (int j = 0; j < image.getWidth(); j++) {
+                pixelsArray[i][j] = image.getRGB(i,j);
+                System.out.print(i+" "+j+" "+Integer.toHexString(pixelsArray[i][j]) + " ");
+            }
+            System.out.println("\n");
+        }
+    }
+    public void decompress(){
+
+    }
 }
