8eeef5dcbbcf924d6770f87bc7d19a4cf54083b1
philokaram <philokrm@gmail.com>
2024-12-13 23:35:25 +0200

    ش

--- VectorQuantization.java
+++ VectorQuantization.java
@@ -1,12 +1,21 @@
 import java.awt.image.BufferedImage;
 
 class VectorQuantization{
-    private int[][] LGBAlgorithm(int[][] arr,int blockWidth , int blockHight , int codeBookSize){
+    private double[][] LGBAlgorithm(int[][] arr,int imageHight , int imageWidth ,int blockWidth , int blockHight , int codeBookSize){
         int[][][] codeBook = new int[blockWidth][blockHight][codeBookSize];
-        int[][] block = new int[blockWidth][blockWidth];
-        for(int i = 0; i < blockHight ; i++){
-            for(int j = 0 ; j < blockWidth ; j++){
+        double[][] block = new double[blockWidth][blockWidth];
+        //1-calculate the average
+        for(int i = 0; i < imageWidth ; i++){
+            for(int j = 0 ; j < imageHight ; j++){
                 block[i%blockHight][j%blockWidth] += arr[i][j];
+                System.out.println(i%blockHight + " "+ j%blockWidth + " "+ i + " "+ j + " "+arr[i][j]);
+            }
+        }
+        double blocksNumber = (imageHight*imageWidth)/(blockHight*blockWidth);
+        System.out.println(blocksNumber);
+        for(int i = 0; i < blockWidth ; i++){
+            for(int j = 0 ; j < blockHight ; j++){
+                block[i][j] /= blocksNumber;
             }
         }
         return block;
@@ -22,7 +31,7 @@ class VectorQuantization{
                 pixelsArray[j][i] = (image.getRGB(j,i) >> 16) & 0xFF;
             }
         }
-        int [][] arr = LGBAlgorithm(pixelsArray, blockWidth, blockHight, codeBookSize);
+        double [][] arr = LGBAlgorithm(pixelsArray,height,width, blockWidth, blockHight, codeBookSize);
         for (int i = 0; i < blockHight; i++) {
             for (int j = 0; j < blockWidth; j++) {
                 System.out.print(arr[j][i]+" ");
