4f1d586153e365be4c535e7a71b1c0ef5b01ce01
philokaram <philokrm@gmail.com>
2025-01-23 20:31:02 +0200

    ش

--- VectorQuantization.java
+++ VectorQuantization.java
@@ -23,17 +23,6 @@ class VectorQuantization{
                 }
             }
         }
-        // for(int n = 0 ; n < this.numberOfBlocksInCodeBook *2; n++){
-        //     for(int i = 0 ; i < this.blockWidth ; i++){
-        //         for(int j = 0 ; j < this.blockHight ; j++){
-        //             System.out.print(newCodeBook[n][i][j]+" ");
-        //         }
-        //         System.out.println();
-        //         System.out.println();
-        //     }
-        //     System.out.println("_____________________________________________");
-        // }
-        // System.out.println("_____________________*________________________");
         return newCodeBook;
     }
 //_____________________________________________________________________________________________________________________________________
@@ -42,7 +31,7 @@ class VectorQuantization{
         for(int i = 0; i < blockWidth && i < imageWidth; i++){
             for(int j = 0 ; j < blockHight && j < imageHight; j++){
                 /*
-                _ _ _ _ _
+                 _ _ _ _ _
                 |         |
                 |  *   *  |
                 |         |
@@ -50,7 +39,6 @@ class VectorQuantization{
                 |_ _ _ _ _|
                 */
                 distance += Math.abs((pixelsArray[(i%blockWidth)+(blockWidth*widthStep)][(j%blockHight)+(blockHight*hightStep)])-(codeBook[codeBookIndex][i][j]));
-                //pixelsArray[(i%blockWidth)+(blockWidth*widthStep)][(j%blockHight)+(blockHight*hightStep)]
             }
         }
         return distance;
@@ -69,12 +57,10 @@ class VectorQuantization{
         //------------------------
         for(int i = 0; i < imageWidth ; i++){
             for(int j = 0 ; j < imageHight ; j++){
-                // System.out.println(i%blockWidth + " "+ j%blockHight + " "+ i + " "+ j + " "+arr[i][j]);
                 initialBlock[i%blockWidth][j%blockHight] += pixelsArray[i][j];
             }
         }
         double blocksNumber = (imageHight*imageWidth)/(blockHight*blockWidth);
-        // System.out.println(blocksNumber);
         for(int i = 0; i < blockWidth ; i++){
             for(int j = 0 ; j < blockHight ; j++){
                 initialBlock[i][j] /= blocksNumber;
@@ -103,7 +89,6 @@ class VectorQuantization{
             frequency = new double[numberOfBlocksInCodeBook];
             widthStep = 0;
             while (widthStep < maxNumberOfWidthSteps) {
-                //System.out.println(widthStep);
                 hightStep = 0;
                 while (hightStep < maxNumberOfHightSteps) {
                     /* 
@@ -132,7 +117,6 @@ class VectorQuantization{
                         }
                     }
                     frequency[minBlockDistanceIndex]++;
-                   // System.out.println("//////////////////////////////////////////////////////////////");
                     hightStep ++;
                 }
                 widthStep ++;
@@ -146,16 +130,6 @@ class VectorQuantization{
             }
             codeBook = averageArray;
         }
-        // for(int n = 0 ; n < this.numberOfBlocksInCodeBook ; n++){
-        //     for(int i = 0 ; i < this.blockWidth ; i++){
-        //         for(int j = 0 ; j < this.blockHight ; j++){
-        //             System.out.print(codeBook[n][i][j]+" ");
-        //         }
-        //         System.out.println();
-        //         System.out.println();
-        //     }
-        //     System.out.println("++++++++++++++++++++++++++++++++++++++++++");
-        // }
         //---------------------------
         //3-check if there changes
         //---------------------------
@@ -188,14 +162,12 @@ class VectorQuantization{
                         }
                     }
                     newFrequency[minBlockDistanceIndex]++;
-                   // System.out.println("//////////////////////////////////////////////////////////////");
                     hightStep ++;
                 }
                 widthStep ++;
             }
             for(int n = 0 ; n < this.numberOfBlocksInCodeBook ; n++){
                 if(newFrequency[n] != frequency[n]){
-                    // System.out.println( frequency[n] +" "+newFrequency[n] );
                     thereAreChanges = true;
                 }
                 for(int i = 0 ; i < this.blockWidth ; i++){
@@ -212,40 +184,10 @@ class VectorQuantization{
 
     }
     public int[][] compress(int[][] pixelsArray ,int imageWidth, int imageHight,int blockWidth , int blockHight , int codeBookSize ){
-        // //-----------------------------------------------------
-        // //1-take the pixels of image and put it in 2d array
-        // //-----------------------------------------------------
-        // int width = image.getWidth();
-        // int height = image.getHeight();
-        // int[][] pixelsArray = new int[width][height];
-        // System.out.println(height+" "+width);
-        // for (int i = 0; i < width; i++) {
-        //     for (int j = 0; j < height; j++) {
-        //         // System.out.println(i+" "+j);
-        //         pixelsArray[i][j] = (image.getRGB(i,j) >> 16) & 0xFF;
-        //     }
-        // }
-        // int[][] temp ={{1,2,7,9,4,11},{3,4,6,6,12,12},{4,9,15,14,9,9},{10,10,20,18,8,8},{4,3,17,16,1,4},{4,5,18,18,5,6}};
-        // for (int i = 0; i < 6; i++) {
-        //     for (int j = 0; j < 6; j++) {
-        //         System.out.print(temp[i][j]+" ");
-        //     }
-        //     System.out.println();
-        // }
-        // double [][][] codeBook = LGBAlgorithm(temp,6,6, blockWidth, blockHight, codeBookSize);
         //-------------------------
-        //2-create the code book
+        //1-create the code book
         //-------------------------
         codeBook = LGBAlgorithm(pixelsArray,imageHight,imageWidth, blockWidth, blockHight, codeBookSize);
-        // for (int n = 0; n < codeBookSize; n++) {
-        //     for (int i = 0; i < blockWidth; i++) {
-        //         for (int j = 0; j < blockHight; j++) {
-        //             System.out.print(codeBook[n][i][j]+" ");
-        //         }
-        //         System.out.println();
-        //     }
-        //     System.out.println("----------------------");
-        // }
         //-------------------------
         //2-create the compressed matrix 
         //-------------------------
@@ -268,7 +210,6 @@ class VectorQuantization{
                 int minBlockDistanceIndex = 0;
                 for(int i = 0 ; i < codeBookSize ; i++ ){
                     double distance = distance(pixelsArray,widthStep,hightStep,codeBook,i);
-                    // double distance = distance(temp,widthStep,hightStep,codeBook,i);
                     if(i == 0){
                         minDistance = distance;
                     }
